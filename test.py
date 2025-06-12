120d76789109148c02dc46dc745c16f9fca9fb26
AyshaAlnuaimi <98530014+AyshaAlnuaimi@users.noreply.github.com>
2025-06-12 14:16:55 +0100

    did little modifications

--- test.py
+++ test.py
@@ -7,9 +7,18 @@ while True:
     if not ret:
         print("Failed to grab frame")
         break
+    
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    
+    # Apply Canny Edge Detection
+    edges = cv2.Canny(frame, threshold1=50, threshold2=150)
 
     # Example processing: just show it
-    cv2.imshow("Pi Camera Stream", frame)
+    cv2.imshow("Original Stream", frame)
+    cv2.imshow("grayscale", gray)
+    cv2.imshow("Canny Edges", edges)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
